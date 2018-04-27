2ef8574c16fb3705f40825fe809c77b4b833eb64
KhanhDuy <truongkhanhduy9@gmail.com>
2018-04-28 00:04:43 +0700

    Resize colored and grayscale image

--- Week1/Excercise.py
+++ Week1/Excercise.py
@@ -24,10 +24,10 @@ def invertAlpha(img):
     return img
 
 def display(img):
-    plt.imshow(cv2.cvtColor(img_new,cv2.COLOR_BGR2RGB))
+    plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
     plt.show()
     
-#Load image from folder
+# Load image from folder
 img = cv2.imread('/Users/truongkhanhduy/Desktop/Project/Personal Github/DeepLearning-in-ComputerVision/Week1/images/image1.png',-1)
 
 # alpha = img[:,:,3] #extract alpha channel
@@ -41,15 +41,36 @@ img = cv2.imread('/Users/truongkhanhduy/Desktop/Project/Personal Github/DeepLear
 # bin[:,:,0] = img[:,:,0]
 #RGB_img = 
 
-#Display image
+# Display image
 display(invertAlpha(img))
 
-#Convert the color image to a gray image, save to a file
+# Convert the color image to a gray image, save to a file
 gray_img = cv2.cvtColor(invertAlpha(img), cv2.COLOR_BGR2GRAY)
 path = "/Users/truongkhanhduy/Desktop/Project/Personal Github/DeepLearning-in-ComputerVision/Week1/images/gray_image.png"
 cv2.imwrite(path, gray_img)
-print("Write Gray-scale Image to file: ", path)
 
-#Display gray image
+# Display gray image
 img_gray_display = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-display(img_gray_display)
+plt.imshow(img_gray_display,cmap='gray')
+plt.show()
+
+# Resize the image to the size of 256 (pixels) x 256 (pixels)
+img_resize = cv2.resize(img, (256, 256), cv2.INTER_LINEAR)
+
+# Display the image
+display(invertAlpha(img_resize))
+
+# Save to a file
+path_resize = "/Users/truongkhanhduy/Desktop/Project/Personal Github/DeepLearning-in-ComputerVision/Week1/images/image_resize.png"
+cv2.imwrite(path_resize, img_resize)
+
+# Resize the gray image to the  size of 256 (pixels) x 256 (pixels)
+gray_img_resize = cv2.resize(gray_img, (256, 256), cv2.INTER_LINEAR)
+
+# Display the image
+plt.imshow(gray_img_resize,cmap='gray')
+plt.show()
+
+# Save to a file
+path_gray_resize = "/Users/truongkhanhduy/Desktop/Project/Personal Github/DeepLearning-in-ComputerVision/Week1/images/gray_image_resize.png"
+cv2.imwrite(path_gray_resize, gray_img_resize)
