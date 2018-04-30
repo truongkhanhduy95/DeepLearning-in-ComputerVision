5bab246021ff6e815a84350c06a8c745ca862f0a
KhanhDuy <truongkhanhduy9@gmail.com>
2018-04-30 11:35:19 +0700

    Split dataset

--- Week2/Exercise2.py
+++ Week2/Exercise2.py
@@ -1,4 +1,6 @@
 # Import packages
+from sklearn import tree
+from sklearn import cross_validation
 import seaborn as sns
 import numpy as np
 from matplotlib import pyplot as plt
@@ -17,4 +19,11 @@ df['petal_width'].plot.hist()
 plt.show()
 sns.pairplot(df, hue='species')
 
+# Split dataset into training data and testing data 
+all_inputs = df[['sepal_length','sepal_width','petal_lenght','petal_width']].values
+all_clases = df[['species']].values
+
+tree.train
 # Build a decision tree classifier to classify this dataset
+clf = tree.DecisionTreeClassifier()
+
